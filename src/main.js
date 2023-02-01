07fd76cf1779ec1f6029f3d6402529018cc4dec2
delta5738 <stlaya641011@gmail.com>
2023-02-02 02:46:12 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+window.dom = {
+    find(selector,scope){
+        return (scope ||document).querySelectorAll(selector);
+    },
+    style(element, property, value){
+        element.style[property] = value;
+    },
+    each(element,fn){
+        for (let i =0;i<element.length;i++){
+            fn(element[i]);
+        }
+    }
+}
+
+const div = dom.find('#test>.red')[0] // 获取对应的元素
+console.log(div)
+dom.style(div, 'color', 'red') // 设置 div.style.color
+
+const divList = dom.find('.red') // 获取多个 div.red 元素
+console.log(divList)
+dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
+
+
